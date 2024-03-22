643c230aa1ca185149661a9334ccff2ed4dc5eb3
PepeKwapien <pppkwapien@gmail.com>
2024-03-22 18:44:37 +0100

    Added slide left animation for collection group headers

--- src/app/components/collection-groups/collection-groups.component.ts
+++ src/app/components/collection-groups/collection-groups.component.ts
@@ -1,3 +1,4 @@
+import { trigger, transition, style, animate } from '@angular/animations';
 import { Component, TemplateRef } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserDocument } from 'src/app/models/documents/user.document';
@@ -7,7 +8,10 @@ import { ModalService } from 'src/app/services/modal/modal.service';
 @Component({
     selector: 'app-collection-groups',
     templateUrl: './collection-groups.component.html',
-    styleUrls: ['./collection-groups.component.scss']
+    styleUrls: ['./collection-groups.component.scss'],
+    animations: [
+        trigger('slideFromLeft', [transition(':enter', [style({ transform: 'translateX(-100%)' }), animate('0.5s ease')])])
+    ]
 })
 export class CollectionGroupsComponent {
     public get user$(): Observable<UserDocument> {
