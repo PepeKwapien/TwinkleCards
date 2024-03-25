ea8fe32396491a6e131b7abf1e374bdc863fc3d1
PepeKwapien <pppkwapien@gmail.com>
2024-03-25 19:23:07 +0100

    Add back arrow

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnDestroy } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnDestroy {
+    private _routingHistory: string[] = [];
+    private _navigationSub: Subscription;
 
+    public get displayBackArrow(): boolean {
+        return this._router.url !== '/home' && this._router.url !== '/';
+    }
+
+    constructor(private _router: Router, private _location: Location) {
+        this._navigationSub = this._router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd) {
+                this._routingHistory.push(event.urlAfterRedirects);
+            }
+        });
+    }
+
+    ngOnDestroy(): void {
+        this._navigationSub.unsubscribe();
+    }
+
+    navigateBack() {
+        this._routingHistory.pop();
+        if (this._routingHistory.length > 0) {
+            this._location.back();
+        } else {
+            this._router.navigateByUrl('/home');
+        }
+    }
 }
+
