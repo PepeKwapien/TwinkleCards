152f409cec26c873e3636f49a9dcbb6cffdc2b2c
PepeKwapien <pppkwapien@gmail.com>
2024-03-28 21:38:50 +0100

    Manage sub manually

--- src/app/components/collection/collection.component.ts
+++ src/app/components/collection/collection.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subscription, combineLatest, filter, shareReplay, switchMap, tap } from 'rxjs';
 import { CollectionDocument } from 'src/app/models/documents/collection.document';
 import { CollectionRepositoryService } from 'src/app/services/collection-repository/collection-repository.service';
 import { UserRepositoryService } from 'src/app/services/user-repository/user-repository.service';
@@ -12,11 +12,38 @@ import { UserRepositoryService } from 'src/app/services/user-repository/user-rep
 export class CollectionComponent implements OnInit, OnDestroy {
     @Input() collectionId!: string;
 
-    public get collection$(): Observable<CollectionDocument | undefined> {
-        return this._collectionRepository.collection$;
+    private _sub: Subscription;
+
+    private _collection: CollectionDocument | undefined;
+    private _username: string | null | undefined;
+
+    public get collection() {
+        return this._collection;
     }
 
-    constructor(private _collectionRepository: CollectionRepositoryService, private _userRepository: UserRepositoryService) {}
+    public get username() {
+        if (this._username === null) {
+            return 'Anonymous';
+        }
+        return this._username;
+    }
+
+    constructor(private _collectionRepository: CollectionRepositoryService, private _userRepository: UserRepositoryService) {
+        this._sub = new Subscription();
+
+        const collection$ = this._collectionRepository.collection$.pipe(filter((collection) => collection != undefined));
+
+        const username$ = collection$.pipe(switchMap((col) => this._userRepository.readUsername(col!.ownerId)));
+
+        const subscription = combineLatest([collection$, username$]).subscribe({
+            next: (value) => {
+                this._collection = value[0];
+                this._username = value[1];
+            }
+        });
+
+        this._sub.add(subscription);
+    }
 
     ngOnInit() {
         this._collectionRepository.setupCollectionChangesListener(this.collectionId);
@@ -24,6 +51,6 @@ export class CollectionComponent implements OnInit, OnDestroy {
 
     ngOnDestroy(): void {
         this._collectionRepository.stopCollectionChangesListener();
+        this._sub.unsubscribe();
     }
 }
-
