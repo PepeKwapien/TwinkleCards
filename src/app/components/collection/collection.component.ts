e8a2ccf385b70ac1fdc8cb7656839e32c8112761
PepeKwapien <pppkwapien@gmail.com>
2024-03-28 21:08:49 +0100

    Clean imports

--- src/app/components/collection/collection.component.ts
+++ src/app/components/collection/collection.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { Observable, distinctUntilChanged, filter, shareReplay, switchMap, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { CollectionDocument } from 'src/app/models/documents/collection.document';
 import { CollectionRepositoryService } from 'src/app/services/collection-repository/collection-repository.service';
 import { UserRepositoryService } from 'src/app/services/user-repository/user-repository.service';
@@ -13,16 +13,8 @@ export class CollectionComponent implements OnInit, OnDestroy {
     @Input() collectionId!: string;
 
     public get collection$(): Observable<CollectionDocument | undefined> {
-        return this._collectionRepository.collection$.pipe(tap((coll) => console.log(coll)));
+        return this._collectionRepository.collection$;
     }
-
-    // public get ownerUsername$(): Observable<string | null | undefined> {
-    //     return this.collection$.pipe(
-    //         distinctUntilChanged(),
-    //         filter((collection) => collection !== undefined),
-    //         switchMap((collection) => this._userRepository.readUsername(collection!.ownerId))
-    //     );
-    // }
 
     constructor(private _collectionRepository: CollectionRepositoryService, private _userRepository: UserRepositoryService) {}
 
