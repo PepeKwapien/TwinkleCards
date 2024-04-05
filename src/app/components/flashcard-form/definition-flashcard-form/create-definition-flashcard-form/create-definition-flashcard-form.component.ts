0222b94f19e0c7aa3db38dc8401ac7fc16fa676d
PepeKwapien <pppkwapien@gmail.com>
2024-04-05 18:30:34 +0200

    Provide definition flashcard service in parent component to reset form

--- src/app/components/flashcard-form/definition-flashcard-form/create-definition-flashcard-form/create-definition-flashcard-form.component.ts
+++ src/app/components/flashcard-form/definition-flashcard-form/create-definition-flashcard-form/create-definition-flashcard-form.component.ts
@@ -4,7 +4,8 @@ import { DefinitionFlashcardService } from 'src/app/services/flashcard/definitio
 @Component({
     selector: 'app-create-definition-flashcard-form',
     templateUrl: './create-definition-flashcard-form.component.html',
-    styleUrls: ['./create-definition-flashcard-form.component.scss']
+    styleUrls: ['./create-definition-flashcard-form.component.scss'],
+    providers: [DefinitionFlashcardService]
 })
 export class CreateDefinitionFlashcardFormComponent {
     constructor(private _flashcardService: DefinitionFlashcardService) {}
--- src/app/services/flashcard/definition-flashcard/definition-flashcard.service.ts
+++ src/app/services/flashcard/definition-flashcard/definition-flashcard.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-@Injectable({
-    providedIn: 'root'
-})
+@Injectable()
 export class DefinitionFlashcardService {
     private _formGroup: FormGroup;
 
