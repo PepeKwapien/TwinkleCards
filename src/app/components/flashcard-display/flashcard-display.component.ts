4c3747df0f06e623965d1ca244c549cb2c83baf9
PepeKwapien <pppkwapien@gmail.com>
2024-05-03 15:26:11 +0200

    Added animation on flashcard slide

--- src/app/components/flashcard-display/flashcard-display.component.ts
+++ src/app/components/flashcard-display/flashcard-display.component.ts
@@ -1,3 +1,4 @@
+import { animate, group, query, style, transition, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import {
     getFrontsideFlashcardHeader,
@@ -6,23 +7,48 @@ import {
     getBacksideFlashcardBody
 } from 'src/app/helpers/flashcard.helper';
 import { IBaseFlashcard } from 'src/app/models/documents/flashcards/base-flashcard.interface';
+import { IFlashcardWithFlipState } from 'src/app/models/flashcard-with-flip-state.interface';
 import { ModalService } from 'src/app/services/modal/modal.service';
+
+const left = [
+    query(':enter, :leave', style({ position: 'absolute' })),
+    group([
+        query(':enter', [
+            style({ transform: 'translateX(-100vw)', opacity: 0 }),
+            animate('0.5s ease-out', style({ transform: 'translateX(0%)', opacity: 1 }))
+        ]),
+        query(':leave', [
+            style({ transform: 'translateX(0%)', opacity: 1 }),
+            animate('0.5s ease-out', style({ transform: 'translateX(100vw)', opacity: 0 }))
+        ])
+    ])
+];
+
+const right = [
+    query(':enter, :leave', style({ position: 'absolute' })),
+    group([
+        query(':enter', [
+            style({ transform: 'translateX(100vw)', opacity: 0 }),
+            animate('0.5s ease-out', style({ transform: 'translateX(0%)', opacity: 1 }))
+        ]),
+        query(':leave', [
+            style({ transform: 'translateX(0%)', opacity: 1 }),
+            animate('0.5s ease-out', style({ transform: 'translateX(-100vw)', opacity: 0 }))
+        ])
+    ])
+];
 
 @Component({
     selector: 'app-flashcard-display',
     templateUrl: './flashcard-display.component.html',
-    styleUrls: ['./flashcard-display.component.scss']
+    styleUrls: ['./flashcard-display.component.scss'],
+    animations: [trigger('slide', [transition(':increment', right), transition(':decrement', left)])]
 })
 export class FlashcardDisplayComponent implements OnInit {
-    @Input({ required: true }) flashcards!: IBaseFlashcard[];
+    @Input({ required: true }) flashcards!: IFlashcardWithFlipState[];
     @Input({ required: true, alias: 'flipState' }) initFlipState!: boolean;
 
     private _index = 0;
-    private _flipped = false;
-
-    public get flipped(): boolean {
-        return this._flipped;
-    }
 
     public get isTherePrevious(): boolean {
         return this._index !== 0;
@@ -32,27 +58,14 @@ export class FlashcardDisplayComponent implements OnInit {
         return this._index !== this.flashcards.length - 1;
     }
 
-    public get frontsideFlashcardHeader(): string {
-        return getFrontsideFlashcardHeader(this.flashcards[this._index]);
-    }
-
-    public get frontsideFlashcardBody(): string {
-        return getFrontsideFlashcardBody(this.flashcards[this._index]);
-    }
-
-    public get backsideFlashcardHeader(): string {
-        return getBacksideFlashcardHeader(this.flashcards[this._index]);
-    }
-
-    public get backsideFlashcardBody(): string {
-        return getBacksideFlashcardBody(this.flashcards[this._index]);
+    public get index(): number {
+        return this._index;
     }
 
     constructor(private _modalService: ModalService) {}
 
     ngOnInit(): void {
         let currentIndex = this.flashcards.length;
-        this._flipped = this.initFlipState;
 
         while (currentIndex != 0) {
             let randomIndex = Math.floor(Math.random() * currentIndex);
@@ -65,29 +78,39 @@ export class FlashcardDisplayComponent implements OnInit {
         }
     }
 
+    public getFrontsideFlashcardHeader(flashcard: IBaseFlashcard): string {
+        return getFrontsideFlashcardHeader(flashcard);
+    }
+
+    public getFrontsideFlashcardBody(flashcard: IBaseFlashcard): string {
+        return getFrontsideFlashcardBody(flashcard);
+    }
+
+    public getBacksideFlashcardHeader(flashcard: IBaseFlashcard): string {
+        return getBacksideFlashcardHeader(flashcard);
+    }
+
+    public getBacksideFlashcardBody(flashcard: IBaseFlashcard): string {
+        return getBacksideFlashcardBody(flashcard);
+    }
+
     public close(): void {
         this._modalService.close();
     }
 
-    public flip(): void {
-        this._flipped = !this._flipped;
+    public flip(flashcardWithFlipState: IFlashcardWithFlipState): void {
+        flashcardWithFlipState.flipped = !flashcardWithFlipState.flipped;
     }
 
     public previous(): void {
         if (this.isTherePrevious) {
-            this._flipped = this.initFlipState;
-            setTimeout(() => {
-                this._index--;
-            }, 300);
+            this._index--;
         }
     }
 
     public next(): void {
         if (this.isThereNext) {
-            this._flipped = this.initFlipState;
-            setTimeout(() => {
-                this._index++;
-            }, 300);
+            this._index++;
         }
     }
 }
