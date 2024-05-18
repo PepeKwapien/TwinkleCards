51ebf062ea07a84eb97ffbf03e8e13f263e282d7
PepeKwapien <pppkwapien@gmail.com>
2024-05-18 15:07:43 +0200

    Manage search input disable state in code not template

--- src/app/components/search-input/search-input.component.ts
+++ src/app/components/search-input/search-input.component.ts
@@ -15,7 +15,7 @@ export class SearchInputComponent implements OnDestroy {
     @Input() sendWithoutEnter = false;
 
     private _sub: Subscription;
-    private _formControl: FormControl = new FormControl('');
+    private _formControl: FormControl = new FormControl({ value: '', disabled: true });
     private _isActive: boolean = false;
 
     public get formControl(): FormControl {
@@ -38,15 +38,40 @@ export class SearchInputComponent implements OnDestroy {
 
     @HostListener('document:click', ['$event'])
     closeOptionsWhenClickedOutside($event: MouseEvent) {
-        const clickedInsideInput = this.checkIfClickedInside($event, this.input);
-        const clickedInsideButton = this.checkIfClickedInside($event, this.button);
+        const clickedInsideInput = this._checkIfClickedInside($event, this.input);
+        const clickedInsideButton = this._checkIfClickedInside($event, this.button);
 
         if (this._isActive && !clickedInsideInput && !clickedInsideButton) {
             this._isActive = false || this.formControl.value.length > 0;
         }
+
+        this._manageInputState();
     }
 
-    private checkIfClickedInside($event: MouseEvent, element: ElementRef) {
+    @HostListener('document:keydown.escape', ['$event'])
+    closeOptions(): void {
+        this._isActive = false || this.formControl.value.length > 0;
+        this._manageInputState();
+    }
+
+    public onIconClick() {
+        if (!this._isActive) {
+            this._isActive = true;
+        } else {
+            this._isActive = false || this.formControl.value.length > 0;
+        }
+
+        this._manageInputState();
+    }
+
+    public onKeyPress($event: KeyboardEvent) {
+        if ($event.key === 'Enter') {
+            $event.preventDefault();
+            this.searchTermEnter.emit(this._formControl.value);
+        }
+    }
+
+    private _checkIfClickedInside($event: MouseEvent, element: ElementRef) {
         const elementDimensions = element.nativeElement.getBoundingClientRect();
         if (
             $event.clientX < elementDimensions.left ||
@@ -60,23 +85,11 @@ export class SearchInputComponent implements OnDestroy {
         return true;
     }
 
-    @HostListener('document:keydown.escape', ['$event'])
-    closeOptions(): void {
-        this._isActive = false || this.formControl.value.length > 0;
-    }
-
-    public onIconClick() {
-        if (!this._isActive) {
-            this._isActive = true;
+    private _manageInputState() {
+        if (this._isActive) {
+            this._formControl.enable();
         } else {
-            this._isActive = false || this.formControl.value.length > 0;
-        }
-    }
-
-    public onKeyPress($event: KeyboardEvent) {
-        if ($event.key === 'Enter') {
-            $event.preventDefault();
-            this.searchTermEnter.emit(this._formControl.value);
+            this._formControl.disable();
         }
     }
 }
