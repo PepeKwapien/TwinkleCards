c6170a2615ea304f682960ad0f081c6c54359bdc
PepeKwapien <pppkwapien@gmail.com>
2024-06-02 11:31:09 +0200

    Remove unused import

--- src/app/components/user-loading-guard/user-loading-guard.component.ts
+++ src/app/components/user-loading-guard/user-loading-guard.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
