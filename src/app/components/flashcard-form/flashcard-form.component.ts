3b402c2f61e4ac21644ac66fa2bd9b22df3c7237
PepeKwapien <pppkwapien@gmail.com>
2024-06-29 14:44:26 +0200

    Improve focusing on flashcard form by using tabulator

--- src/app/components/flashcard-form/flashcard-form.component.ts
+++ src/app/components/flashcard-form/flashcard-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, TemplateRef } from '@angular/core';
+import { AfterViewInit, Component, Input, TemplateRef } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Component({
@@ -6,29 +6,83 @@ import { FormGroup } from '@angular/forms';
     templateUrl: './flashcard-form.component.html',
     styleUrls: ['./flashcard-form.component.scss']
 })
-export class FlashcardFormComponent {
+export class FlashcardFormComponent implements AfterViewInit {
     @Input({ required: true }) formTitle!: string;
     @Input({ required: true }) formGroup!: FormGroup;
     @Input({ required: true }) buttonTemplate!: TemplateRef<Element>;
     @Input({ required: true }) buttonCallback!: () => Promise<void>;
 
-    public formFlipped: boolean = false;
+    public get formFlipped(): boolean {
+        return this._formFlipped;
+    }
+
+    public _formFlipped: boolean = false;
 
     public get isOtherSideInvalid(): boolean {
-        if (this.formFlipped) {
+        if (this._formFlipped) {
             return !this.formGroup.get('frontside')?.valid;
         } else {
             return !this.formGroup.get('backside')?.valid;
         }
     }
 
+    ngAfterViewInit(): void {
+        this._focusOnFrontside();
+    }
+
     public flip() {
-        this.formFlipped = !this.formFlipped;
+        this._formFlipped = !this._formFlipped;
+
+        if (!this._formFlipped) {
+            this._focusOnFrontside();
+        } else {
+            this._focusOnBackside();
+        }
     }
 
     public async callback($event: Event) {
         $event.preventDefault();
         await this.buttonCallback();
+    }
+
+    private _focusOnFrontside(): void {
+        // unfocus backside
+        if (document.getElementById('backsideFirst') != null) {
+            document.getElementById('backsideFirst')!.tabIndex = -1;
+        }
+        if (document.getElementById('backsideSecond')) {
+            document.getElementById('backsideSecond')!.tabIndex = -1;
+        }
+
+        // allow focusing frontside
+        if (document.getElementById('frontsideFirst') != null) {
+            document.getElementById('frontsideFirst')!.tabIndex = 0;
+        }
+        if (document.getElementById('frontsideSecond')) {
+            document.getElementById('frontsideSecond')!.tabIndex = 0;
+        }
+
+        document.getElementById('frontsideFirst')?.focus();
+    }
+
+    private _focusOnBackside(): void {
+        // allow focusing backside
+        if (document.getElementById('backsideFirst') != null) {
+            document.getElementById('backsideFirst')!.tabIndex = 0;
+        }
+        if (document.getElementById('backsideSecond')) {
+            document.getElementById('backsideSecond')!.tabIndex = 0;
+        }
+
+        // unfocus frontside
+        if (document.getElementById('frontsideFirst') != null) {
+            document.getElementById('frontsideFirst')!.tabIndex = -1;
+        }
+        if (document.getElementById('frontsideSecond')) {
+            document.getElementById('frontsideSecond')!.tabIndex = -1;
+        }
+
+        document.getElementById('backsideFirst')?.focus();
     }
 }
 
